0841318453239bc2f46e7090bead6fa82e61c32c
shibadog <shibadog99@gmail.com>
2020-05-24 18:50:05 +0900

    thymeleafを導入

---
+++ src/main/java/com/github/shibadog/springbootsession/web/BoardController.java
@@ -0,0 +1,14 @@
+package com.github.shibadog.springbootsession.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    
+    @GetMapping({ "/", "" })
+    public String index(Model model) {
+        return "index";
+    }
+}

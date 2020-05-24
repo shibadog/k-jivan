cf5d2fe8e50a69a04862df20476c1919a5d35b0b
shibadog <shibadog99@gmail.com>
2020-05-24 20:33:55 +0900

    layout-dialectを追加

--- src/main/java/com/github/shibadog/springbootsession/web/BoardController.java
+++ src/main/java/com/github/shibadog/springbootsession/web/BoardController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
-    
     @GetMapping({ "/", "" })
     public String index(Model model) {
         return "index";

2d881e859de6dd926995dd2f4b0f3fcf51764888
shibadog <shibadog99@gmail.com>
2020-05-24 16:20:22 +0900

    spring boot initializerで作り直し

---
+++ src/main/java/com/github/shibadog/springbootsession/SpringBootSessionApplication.java
@@ -0,0 +1,13 @@
+package com.github.shibadog.springbootsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionApplication.class, args);
+	}
+
+}
